3bda33bbc0d93f8e59cd2b528811c34d796c09d2
프루시안블루 <frusianblue@gmail.com>
2026-08-13 22:47:35 +0900

    운영 규칙 추가

--- projects/eos-asis-replace/artifacts/Folder-Check.ps1
+++
@@ -1,5 +0,0 @@
-﻿Import-Csv .\report\portal_asis.dat |
-  Where-Object { $_.FoundIn -eq 'FILE' } |
-  ForEach-Object { [regex]::Matches($_.Match, '[dD]:[/\\]+[^/\\";''<>\s,)]*[/\\]?[^/\\";''<>\s,)]*') } |
-  ForEach-Object { ($_.Value -replace '[\\/]+','/').ToLower() } |
-  Sort-Object -Unique
--- projects/eos-asis-replace/artifacts/Ip-Check.ps1
+++
@@ -1,5 +0,0 @@
-﻿Import-Csv .\report\portal_ip_all.dat |
-  Where-Object { $_.FoundIn -eq 'FILE' } |
-  ForEach-Object { [regex]::Matches($_.Match, '(?:\d{1,3}\.){3}\d{1,3}') } |
-  ForEach-Object { ($_.Value -replace '[\\/]+','/').ToLower() } |
-  Sort-Object -Unique
